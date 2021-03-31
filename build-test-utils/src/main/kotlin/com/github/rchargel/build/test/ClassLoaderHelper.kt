630061d5ee20192de8cac4832f9c7d9417b21f81
rchargel <rchargel@zcarioca.net>
2021-03-31 18:17:58 -0400

    Just make it a singleton

--- build-test-utils/src/main/kotlin/com/github/rchargel/build/test/ClassLoaderHelper.kt
+++ build-test-utils/src/main/kotlin/com/github/rchargel/build/test/ClassLoaderHelper.kt
@@ -2,13 +2,11 @@ package com.github.rchargel.build.test
 
 import java.net.URLClassLoader
 
-class ClassLoaderHelper {
-    companion object {
-        @JvmStatic
-        fun  addClassToClassLoader(vararg types: Class<*>) {
-            val locations = types.mapNotNull { it?.protectionDomain?.codeSource?.location }.toTypedArray()
-            val classLoader = URLClassLoader.newInstance(locations, Thread.currentThread().contextClassLoader);
-            Thread.currentThread().contextClassLoader = classLoader;
-        }
+object ClassLoaderHelper {
+    @JvmStatic
+    fun addClassToClassLoader(vararg types: Class<*>) {
+        val locations = types.map { it.protectionDomain.codeSource.location }.toTypedArray()
+        val classLoader = URLClassLoader.newInstance(locations, Thread.currentThread().contextClassLoader)
+        Thread.currentThread().contextClassLoader = classLoader
     }
 }
